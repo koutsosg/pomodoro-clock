7285c68d9bb39901be5cacb7e65c137bd3676ceb
koutsosg <g.koutsos123@gmail.com>
2022-11-06 14:40:44 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header ">
+        <div className="container">
+          <div className="row">
+            <div className="my-5">
+              <h1>Pomodoro Clock</h1>
+            </div>
+
+            <div className="col-6">
+              <h3 id="break-label"> Break Length</h3>
+              <button>-</button>
+              <span>5</span>
+              <button>+</button>
+            </div>
+            <div className=" col-6">
+              <h3 id="session-label"> Session Length</h3>
+              <button>-</button>
+              <span>25</span>
+              <button>+</button>
+            </div>
+            <div className="">
+              <div>
+                <h3>Session</h3>
+                <span>24:00</span>
+              </div>
+              <div>
+                <button type="">Start</button>
+                <button type="">Pause</button>
+                <button type="">Refreash</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
